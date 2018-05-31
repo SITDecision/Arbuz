3a098110aa28fc7f358952dd6df70b2669f7c62b
Иван Городилов <sneik108@gmail.com>
2018-05-31 21:29:12 +0300

    Fixed work for product + some localizations

--- Arbuz/Data/Entities/Product.cs
+++ Arbuz/Data/Entities/Product.cs
@@ -1,9 +1,13 @@
-﻿namespace Arbuz.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Arbuz.Data.Entities
 {
     public class Product
     {
         public int Id { get; set; }
+        [Display(Name = "Тип операции")]
         public ServiceType ServiceType { get; set; }
+        [Display(Name = "Название")]
         public string Name { get; set; }
     }
 }
--- Arbuz/Data/Entities/ServiceType.cs
+++ Arbuz/Data/Entities/ServiceType.cs
@@ -1,11 +1,18 @@
-﻿namespace Arbuz.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Arbuz.Data.Entities
 {
     public enum ServiceType
     {
+        [Display(Name = "Фото")]
         Photo,
+        [Display(Name = "Ксерокс")]
         Xerox,
+        [Display(Name = "Печать")]
         Print,
+        [Display(Name = "Ламинирование")]
         Lamination,
+        [Display(Name = "Товары")]
         Goods
     }
 }
