36217bde730e99b5171336901937ddeaba99c0b8
Иван Городилов <sneik108@gmail.com>
2018-06-02 00:15:06 +0300

    Added a summary report

---
+++ Arbuz/DTO/OperationInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Arbuz.DTO
+{
+    public class OperationInfo
+    {
+        [Display(Name = "Товар/услуга")]
+        public string ProductName { get; set; }
+        [Display(Name = "Цена")]
+        public decimal? Price { get; set; }
+        [Display(Name = "Количество")]
+        public int Quantity { get; set; }
+        [Display(Name = "Сумма")]
+        public decimal? Sum => Price.HasValue ? Price * Quantity : null;
+        [Display(Name = "Дата")]
+        public string Date { get; set; }
+    }
+}
---
+++ Arbuz/Pages/CopyCenter/Summary/Index.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using Arbuz.Data;
+using Arbuz.Data.Entities;
+using Arbuz.DTO;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Arbuz.Pages.CopyCenter.Summary
+{
+    public class SummaryModel : PageModel
+    {
+        private readonly Arbuz.Data.ApplicationDbContext _context;
+
+        public SummaryModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<OperationInfo> OperationInfoGroupedByDateAndProduct { get; private set; }
+
+        public async Task OnGetAsync()
+        {
+            var operationsInfo = await _context.Operations
+                .Include(o => o.Product)
+                .Select(o => GetOperationInfo(o))
+                .ToListAsync();
+            OperationInfoGroupedByDateAndProduct = (from operation in operationsInfo
+                group operation by new {operation.Result.Date, operation.Result.ProductName}
+                into gr
+                select new OperationInfo
+                {
+                    Date = gr.Key.Date,
+                    ProductName = gr.Key.ProductName,
+                    Price = gr.Sum(p => p.Result.Price),
+                    Quantity = gr.Sum(p => p.Result.Quantity)
+                })?.OrderBy(o => o.Date);
+        }
+
+        private async Task<OperationInfo> GetOperationInfo(Operation operation)
+        {
+            var correspondingPrice = await _context.Prices.OrderByDescending(p => p.Date)
+                .FirstOrDefaultAsync(p => p.Date <= operation.Date && p.ProductId == operation.ProductId);
+            return
+                new OperationInfo
+                {
+                    Price = correspondingPrice?.Value,
+                    Quantity = operation.Quantity,
+                    ProductName = operation.Product.Name,
+                    Date = operation.Date.ToShortDateString()
+                };
+        }
+    }
+}
